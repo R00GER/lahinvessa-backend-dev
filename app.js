891fe50e0e080ef5b604a3feed54be1f16355466
R00GER <roni.friman@sonit.fi>
2020-12-05 12:29:12 +0200

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.set('useFindAndModify', false);
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.mfcfk.mongodb.net/lahinvessaDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.mfcfk.mongodb.net/lahinvessaDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log('connected to db');
 })
 
@@ -30,6 +30,7 @@ app.use('/api/locations', locationsRouter);
 app.use('/api/locations/pending', pendingLocationsRouter);
 
 const PORT = process.env.PORT || 3001;
+console.log(PORT);
 
 app.listen(PORT, () => {
   console.log('listening to port 3001');
